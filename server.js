6ab6027552c2a072f0ac42da7848427a8f0fc44f
gizzard <adamumohammed2477@gmail.com>
2021-06-14 16:03:33 +0100

    made some changes

--- server.js
+++ server.js
@@ -23,4 +23,5 @@ app.get("/api/items", (req, res) => {
     res.json({items})
 })
 
-app.listen(5000, ()=>console.log("server running on port 5000"))
+const PORT= process.env.PORT || 5000
+app.listen(PORT, ()=>console.log("server running on port 5000"))
